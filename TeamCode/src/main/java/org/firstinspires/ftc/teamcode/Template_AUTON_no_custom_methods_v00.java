b430b72887ea894021f1b6e5cbc4aedce902880e
Steve Derry <stevenwderry@gmail.com>
2018-09-28 15:13:55 -0400

    remove custom methods, edit comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Template_AUTON_no_custom_methods_v00.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Template_AUTON_no_custom_methods_v00.java
@@ -1,18 +1,26 @@
 //*************************************************************************************************************************
 //***************************************** THIS FILE IS A TEACHING TEMPLATE **********************************************
-// Edit Date:   September 28, 2018 @ 13:46
+// Edit Date:   September 28, 2018 @ 15:13
 // Clone Date:  September 28, 2018 @ 13:43
 // Team Name:   _____
 // Team Number: _____
 // Code Type:   OpMode for AUTONOMOUS
 // Description: Brief description of what this code does:
-//              1. Start at predetermined location (positioned by drivers prior to game start)
-//              2. Drive forward to make contact with game object, then pause to let object flex/bounce/roll/slide
-//              3. Spin left to push object off its original position, then pause to let object flex/bounce/roll/slide
-//              4. Spin right to return to original orientation and prepare to park on object's original location
-//              5. Drive forward onto object's original location, then stop
-//              6. Set gripper to open position
-//              7. Wait for Teleop
+//               0.  Start at predetermined location (positioned by drivers prior to game start)
+//               1.  Drive FORWARD, FAST, for 3 seconds
+//               2.  STOP
+//               3.  Spin LEFT, SLOWLY, for 1 second
+//               4.  STOP
+//               5.  Drive FORWARD, SLOWLY, for 2 seconds
+//               6.  STOP
+//               7.  Spin RIGHT, SLOWLY, for 2 seconds
+//               8.  STOP
+//               9.  Extend arm OUTWARD, SLOWLY, for 1 second
+//              10.  STOP arm
+//              11.  Set gripper to OPEN position
+//              12.  Retract arm INWARD, SLOWLY, for 1 second
+//              13.  STOP
+//              14.  Wait for Teleop
 //*************************************************************************************************************************
 //*************************************************************************************************************************
 //
@@ -30,24 +38,24 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 //      2. Utilities (specific)
 import com.qualcomm.robotcore.util.ElapsedTime;
 //
-//      3. Hardware (types: ONE import per TYPE of hardware, NOT for each INSTANCE of that type of hardware)
+//      3. Hardware (types: ONE import per TYPE of hardware, NOT for each INSTANCE of that TYPE of hardware)
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 // 
 // DEFINE OpMode
 // NOTE:    OpMode is the name for a set of code that contains the instructions the robot will perform. It is a class
 //          inside the ftc_app code package supplied by FTC.
-// FORMAT:  @type(name="OpMode_Name",group="GroupName") 
-@Autonomous(name="Template_AUTON_no_methods_v00", group="Derry_FTC_Templates")
+// FORMAT:  @type(name="OpMode_Name", group="GroupName") 
+@Autonomous(name="Template_AUTON_no_custom_methods_v00", group="Derry_FTC_Templates")
 //
 // DEFINE class
 // NOTE:    All JAVA files must have at least one CLASS
-// FORMAT:  access level, class class_name, extends name of class this new class extends (if any) {
-public class Template_AUTON_no_methods_v00 extends LinearOpMode {
+// FORMAT:  access level, class class_name, extends NameOfClass this new class extends (if any) {
+public class Template_AUTON_no_custom_methods_v00 extends LinearOpMode {
     //
     // DECLARE OpMode MEMBERS
     // 1. Utilities
-    //    FORMAT:   access level, UtilityName = new UtilityName(); starting value
+    //    FORMAT:   access level, UtilityName runtime = new UtilityName();
     private ElapsedTime runtime = new ElapsedTime();        // Use private unless you need access from other classes.
     //
     // 2. Hardware (DECLARE and INTIALIZE variables at the same time)
@@ -61,18 +69,16 @@ public class Template_AUTON_no_methods_v00 extends LinearOpMode {
     //    FORMAT:   HardwareType hardwareVariableName = hardwareMap.hardwareType.get("nameAsAssignedInHardwareMap");             
     DcMotor leftDriveMotor  = hardwareMap.dcMotor.get("leftDriveMotor");
     DcMotor rightDriveMotor = hardwareMap.dcMotor.get("rightDriveMotor");
-    DcMotor sweeperMotor    = hardwareMap.dcMotor.get("sweeperMotor");                        
     DcMotor armMotor        = hardwareMap.dcMotor.get("armMotor");                        
-    Servo   gripperServo    = null;                    
+    Servo   gripperServo    = hardwareMap.servo.get("gripperServo";                    
     //
     // SET DC MOTOR DIRECTIONS
-    // NOTE:    "Reverse" any motor that runs backwards (relative to "forward" direction of robot)
-    //          when powered by a positive power value
+    // NOTE:    "Reverse" any motor that runs backwards (relative to desired "forward" motion of element pwoered
+    //          by that motor, such as a drive wheel or extension arm) when powered by a positive power value
     // FORMAT:  hardwareName.setDirection(DcMotor.Direction.DIRECTION)
     leftDriveMotor.setDirection(DcMotor.Direction.REVERSE);     
     rightDriveMotor.setDirection(DcMotor.Direction.FORWARD);    
-    weeperMotor.setDirection(DcMotor.Direction.FORWARD);       // Assumes sweeperMotor is same orientation as rightDriveMotor
-    //
+    armMotor.setDirection(DcMotor.Direction.FORWARD);
     //
     // DEFINE CODE CONSTANTS
     // NOTE:    CONSTANTS should generally be defined here (outside of METHOD bodies),
@@ -97,28 +103,15 @@ public class Template_AUTON_no_methods_v00 extends LinearOpMode {
     //          - final means its value never changes (constant)
     //          - long, double, etc. is the type of value held by the variable
     //
-    // Drive times: all values are in milliseconds, and all values assume motors are using DRIVE_POWER_FAST value
-    public static final long DRIVE_TIME_TO_OBJECT       = 10000;
-    public static final long DRIVE_TIME_OBJECT_TO_BASE  = 2000;         
-    public static final long DRIVE_TIME_45_DEG_TURN     = 1000;
-    public static final long DRIVE_TIME_90_DEG_TURN     = DRIVE_TIME_45_DEG_TURN * 2;
-    //
     // Drive powers (speeds): all values use range of 0 to 1
-    public static final double DRIVE_POWER_FAST                     = .8;
-    public static final double DRIVE_POWER_ADJUSTER                 = 2;
-    public static final double DRIVE_POWER_MEDIUM                   = DRIVE_POWER_FAST / DRIVE_POWER_ADJUSTER;
-    public static final double DRIVE_POWER_SLOW                     = DRIVE_POWER_FAST / (DRIVE_POWER_ADJUSTER * 2);
-    public static final double DRIVE_TIME_ADJUSTER_FOR_POWER_MED    = DRIVE_POWER_ADJUSTER;
-    public static final double DRIVE_TIME_ADJUSTER_FOR_POWER_SLOW   = DRIVE_POWER_ADJUSTER * 2;
+    public static final double MOTOR_STOP               = 0;
+    public static final double DRIVE_POWER_FAST         = 0.8;
+    public static final double DRIVE_POWER_SLOW         = DRIVE_POWER_FAST / 2);
+    public static final double ARM_MOTOR_MOVE           = .2;
     //
     // Position address values for servos
-    public static final double GRIPPER_SERVO_START      = 10;           // Open and stowed to fit inside 18" sizing cube
-                                                                        //      but not clear of other moving parts of robot
-    public static final double GRIPPER_SERVO_REST       = 50;           // Open and fully clear of all moving parts of robot
-    public static final double GRIPPER_SERVO_OPEN       = 100;          // Open to receive game elements
-    public static final double GRIPPER_SERVO_GRIP_LG    = 150;          // Closed to grip large game elements
-    public static final double GRIPPER_SERVO_GRIP_SM    = 200;          // Closed to grip small game elements
-    public static final double GRIPPER_SERVO_CLOSED     = 250;          // Closed completely
+    public static final double GRIPPER_SERVO_OPEN       = 0;            // Open to receive game elements
+    public static final double GRIPPER_SERVO_CLOSED     = 200;          // Closed to grip game elements
     //
     @Override
     // Override is a note to the compiler stating that you expect that you are replacing a METHOD
@@ -140,49 +133,84 @@ public class Template_AUTON_no_methods_v00 extends LinearOpMode {
         //
         // SET ALL MOTORS TO DESIRED STARTING STATUS
         //      DC Motors
-        stopRobot();                                                    // Use METHOD call to set all DC motors to STOP
+        leftDriveMotor.setPower(0);
+        rightDriveMotor.setPower(0);                            
+        armMotor.setPower(0);      
+        //
         //      Servo Motors
-        gripperServo.setPosition(GRIPPER_SERVO_START);                  // Set SERVO motor to desired start position
-                                                                        //      using variable defined above
+        gripperServo.setPosition(GRIPPER_SERVO_CLOSED);                 // Set SERVO motor to desired start position
+                                                                        //      (gripping item) using a
+                                                                        //      variable defined above
         //
         //****************************************************************************************************************
         // END OF PREPARATIONS
         //****************************************************************************************************************
         //
         // WAIT for driver to press PLAY
-        waitForStart();                     // The waitForStart() METHOD is part of the LinearOpMode CLASS,
-                                            //      which is defined elsewhere in the FTC resource code
+        waitForStart();                                                 // The waitForStart() METHOD is part of
+                                                                        //    the LinearOpMode CLASS,
+                                                                        //    which is defined elsewhere in
+                                                                        //    the FTC resource code
         //
         //****************************************************************************************************************
         // AFTER driver presses PLAY, execute code below this line
         //****************************************************************************************************************
         //
-        // 1. Start at predetermined location (positioned by drivers prior to game start)
+        //   0.  Start at predetermined location (positioned by drivers prior to game start)
+        //   1.  Drive FORWARD, FAST, for 3 seconds
+        leftDriveMotor.setPower(DRIVE_POWER_FAST);
+        rightDriveMotor.setPower(DRIVE_POWER_FAST);
+        sleep(3000);                                            //  Sleep for 3000 miliseconds (3 seconds)
+                                                                //  Sleep(X) pauses code execution so that motors
+                                                                //      can turn for the time entered as X                                                                
         //
-        // 2. Drive forward to make contact with game object, then pause to let object flex/bounce/roll/slide
-        driveForward(DRIVE_TIME_TO_OBJECT,DRIVE_POWER_FAST);   // Arguments MUST be in order expected by method
-        stopRobot();                                            // Stop      
-        sleep((long) 2);                                        // 2 seconds allows Object to bounce/flex before rogbot moves again
+        //   2.  STOP
+        leftDriveMotor.setPower(MOTOR_STOP);                    //  You could enter 0 here instead of the variable MOTOR_STOP,
+        rightDriveMotor.setPower(MOTOR_STOP);                   //      but this keeps the code readable    
         //
-        // 3. Spin left to push object off its base, then pause to let object flex/bounce/roll
-        spinLeft(DRIVE_TIME_45_DEG_TURN,DRIVE_POWER_MEDIUM);    // Spin 45 deg. to left
-        stopRobot();                                            // Stop        
-        sleep((long) 2);                                        // 2 seconds allows Object to bounce/flex before rogbot moves again
+        //   3.  Spin LEFT, SLOWLY, for 2 seconds
+        leftDriveMotor.setPower(-DRIVE_POWER_FAST);             //  Negative value to make motor spin wheel backward
+        rightDriveMotor.setPower(DRIVE_POWER_FAST);             //  When left motor spins backward, and
+                                                                //      right motor spins forward, then the
+                                                                //      robot will spin to its left
+        sleep(1000);                                            //  Run motors for 1 second
         //
-        // 4. Spin right to prepare to park on object's original location
-        spinRight(DRIVE_TIME_45_DEG_TURN,DRIVE_POWER_MEDIUM);   // Spin 45 deg. to right to return to original orientation
+        //   4.  STOP
+        leftDriveMotor.setPower(MOTOR_STOP); 
+        rightDriveMotor.setPower(MOTOR_STOP);
         //
-        // 5. Drive forward onto object's original location, then stop
-        driveForward(DRIVE_TIME_OBJECT_TO_BASE,DRIVE_POWER_SLOW);    
-        stopRobot();                                            // Final stop until beginning of Teleop
+        //   5.  Drive FORWARD, SLOWLY, for 2 seconds
+        leftDriveMotor.setPower(DRIVE_POWER_SLOW);
+        rightDriveMotor.setPower(DRIVE_POWER_SLOW);
+        sleep(2000);                                            //  Run motors for 2 seconds
         //
-        // 6. If any parts of robot need to be repositioned (arms, etc.) to prepare for Teleop,
-        //    place that code here before next "}" character
+        //   6.  STOP
+        leftDriveMotor.setPower(MOTOR_STOP); 
+        rightDriveMotor.setPower(MOTOR_STOP);
         //
-        // Open gripper hand
+        //   7.  Spin RIGHT, SLOWLY, for 2 seconds
+        leftDriveMotor.setPower(DRIVE_POWER_FAST);  
+        rightDriveMotor.setPower(-DRIVE_POWER_FAST);            //  Negative value to make motor spin wheel backward
+                                                                //  When right motor spins backward, and
+                                                                //      left motor spins forward, then the
+                                                                //      robot will spin to its right
+        //   8.  Extend arm OUTWARD, SLOWLY, for 1 second
+        armMotor.setPower(ARM_MOTOR_MOVE); 
+        sleep(1000);                                            //  Run motor for 1 second
+        //
+        //   9.  STOP arm
+        armMotor.setPower(MOTOR_STOP); 
+        //
+        //  10.  Set gripper to OPEN position
         gripperServo.setPosition(GRIPPER_SERVO_OPEN);
         //
-        // 7. Wait for Teleop
+        //  11.  Retract arm INWARD, SLOWLY, for 1 second
+        armMotor.setPower(-ARM_MOTOR_MOVE);                     //  Negative value to make gear that moves arm, 
+                                                                //      and retracts arm
+        sleep(1000);                                            //  Run motor for 1 second
+        //  12.  STOP arm
+        //
+        //  13.  Wait for Teleop
     }
     // END of METHOD runOpMode
     //
