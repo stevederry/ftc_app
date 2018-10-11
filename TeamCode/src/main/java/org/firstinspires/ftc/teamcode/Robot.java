504fb67f7adbe49855cbef4828f55b485ae71f5c
Steve Derry <stevenwderry@gmail.com>
2018-10-11 14:08:41 -0400

    define code pkg, import hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -1,6 +1,44 @@
-// IMPORT CODE PACKAGE
-
-// DECLARE CLASS
+// *************************************************************************************************************************
+// *************************************************************************************************************************
+// Edit Date:   October 11, 2018 @ 14:04
+// Clone Date:  October 11, 2018 @ 14:02
+// Team Name:   Lightning Robotics
+// Team Number: FRC862
+// Code Type:   definition and initial setup of all robot hardware:
+//              - DC motors
+//              - Servo motors  (none present yet)
+//              - Sensors       (none present yet)
+// NOTE:        This code is based on AUTON_withExternalConstants_v00
+// *************************************************************************************************************************
+// *************************************************************************************************************************
+//
+// DEFINE CODE PACKAGE
+package org.firstinspires.ftc.teamcode;
+//
+// IMPORT PROGRAMMING ELEMENTS DESCRIBED ELSEWHERE IN THE CODE PACKAGE FOR USE IN THIS FILE
+//      1. Classes
+//         none
+//
+//      2. Utilities
+//         none
+//
+//      3. Hardware Types (ONE import per TYPE of hardware, NOT for each INSTANCE of that TYPE of hardware)
+import com.qualcomm.robotcore.hardware.DcMotor;
+//         import Servos, Sensors, etc. here
+//
+//
+// DEFINE class
+// FORMAT:  access level, class class_name, extends NameOfClass_this_new_class_extends_(if_any) {
+public class Robot {
+    // DECLARE OpMode MEMBERS
+    // 1. Utilities
+    //    none
+    //
+    // 2. Hardware
+    //    NOTE:     This section tells the code that, later, these names will be used to refer to items on the robot.
+    //    FORMAT:   HardwareType variableName;
+    DcMotor leftDriveMotor;
+    DcMotor rightDriveMotor;
 
 
 
